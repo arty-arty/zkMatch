1cb998eb62eb23c5be593cc142886070a368023b
Arthem <very@secret.org>
2023-04-22 03:03:21 +0700

    Try to avoid excessive optimization

--- ark-serializer/pkg/ark_serializer.js
+++ ark-serializer/pkg/ark_serializer.js
@@ -1,4 +1,4 @@
 import * as wasm from "./ark_serializer_bg.wasm";
 import { __wbg_set_wasm } from "./ark_serializer_bg.js";
-__wbg_set_wasm(wasm);
+const hahaDontTrickMe = __wbg_set_wasm(wasm);
 export * from "./ark_serializer_bg.js";
